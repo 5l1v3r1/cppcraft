fc8f0afef184255873e873e13ec5050bab1ba598
fwsGonzo <fwsgonzo@hotmail.com>
2017-10-15 12:15:57 +0200

    Cleanup biomegen and update TODO

--- src/generator/biomegen/biomegen.cpp
+++ src/generator/biomegen/biomegen.cpp
@@ -8,53 +8,6 @@
 
 namespace terragen
 {
-	static const int BT_AXES = 8;
-
-	// 2D biomes
-	uint16_t biomeTable[BT_AXES+1][BT_AXES+1] =
-	{
-		{  0, 0, 0, 1, 1, 1, 1, 2,  7 },
-		{  1, 2, 2, 3, 4, 5, 6, 7,  7 },
-		{  2, 2, 3, 4, 5, 5, 6, 6,  6 },
-		{ 10,11, 3, 5, 5, 8, 7, 8,  8 },
-		{ 12,12, 7, 5, 6, 7, 8, 9,  9 },
-		{ 13,12, 9,10,11,12, 9,10, 10 },
-		{ 13,19,18,15,15, 9,16,20, 20 },
-		{ 14,13,18,15,16,16,20,17, 17 },
-		{ 14,13,18,15,16,16,20,17, 17 }
-	};
-
-	const int cols[21][3] =
-	{
-		{ 232,232,232},  // ice
-		{ 89, 92, 76 },  // polar stone/ice desert
-		{ 66, 72, 46 },  // beginning tundra
-		{ 77, 76, 46 },  // tundra
-
-		{ 85, 83, 47 },  // tundra to boreal
-		{ 43, 50, 20 },  // boreal 1
-		{ 48, 66, 23 },  // boreal 2
-		{ 51, 63, 24 },  // hemiboreal 1
-
-		{ 30, 51, 13 },  // hemiboreal 2
-		{ 56, 79, 26 },  // temperate
-		{ 66, 92, 33 },  // Mediterranean 1
-		{ 79, 92, 41 },  // Mediterranean 2
-
-		{ 107,105, 56 },  // subtropical dry
-		{ 200,177,144 },  // desert
-		{ 211,157,111 },  // red desert
-
-		{  70, 68, 37 },  // wet/dry savanna
-
-		{  35, 48, 16 },  // monsoon
-		{  36, 85, 19 },  // rainforest
-		{ 156,138, 89 },  // dry
-
-		{ 167,150,116 },  // chinese desert
-		{  31, 65, 13 }   // amazonas rf
-	};
-
 	Biome::result_t Biome::biomeGen(glm::vec2 pos)
 	{
 		const float climateBias = 0.9f; // <1.0 more warm, >1.0 more cold
@@ -63,7 +16,7 @@ namespace terragen
 		float b2 = 0.5f + 0.45f*glm::simplex(pos*1.0f) + 0.05f*glm::simplex(pos*7.0f) + 0.025*glm::simplex(pos*14.0f);
 
 		b1 = powf(b1, climateBias);
-		b1 /= 0.95; // b1 only reaches 0.95
+		b1 /= 0.95f; // b1 only reaches 0.95
     b1 = glm::clamp(b1, 0.0f, 1.0f) * 40.0f;  // temperature
     b2 = glm::clamp(b2, 0.0f, 1.0f) * 400.0f; // precipitation
 
